bdb1715cc473664dc4e1842c01aa047a3b588c4a
laresrocio <laresrociod@gmail.com>
2026-04-30 11:50:35 -0300

    CREATE: creacion de switch con mensajes provisorios en index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const params = process.argv.slice(2);
+const operacion = params[0];
+
+let resultado;
+
+const main = async () => {
+  switch (operacion) {
+    case "get":
+      resultado = "aca se listan los usuarios de la db"
+      break;
+    case "add":
+      resultado = "aca se agrega un usuario a la db"
+      break;
+    case "update":
+      resultado = "aca se actualiza un usuario de la db"
+      break;
+    case "delete":
+      resultado = "aca se borra un usuario de la db"
+      break;
+    default:
+      resultado = "operacion invalida"
+      break;
+  }
+  console.log(resultado);
+
+  process.exit();
+}
+main();
